b0e61739488b717bf58a2b4f89f8ea38009fcf70
siribins <siribins@gmail.com>
2025-03-26 22:22:00 +0530

    Module 2

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -62,7 +62,10 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join("/mnt/c/Users/sirib/Desktop/ibm/xrwvm-fullstack_developer_capstone/server/",'frontend/static')
+            os.path.join("/mnt/c/Users/sirib/Desktop/ibm/xrwvm-fullstack_developer_capstone/server/",'frontend/static'),
+            os.path.join("/mnt/c/Users/sirib/Desktop/ibm/xrwvm-fullstack_developer_capstone/server/",'frontend/build'),
+            os.path.join("/mnt/c/Users/sirib/Desktop/ibm/xrwvm-fullstack_developer_capstone/server/",'frontend/build/static')
+
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -137,6 +140,8 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join("/mnt/c/Users/sirib/Desktop/ibm/xrwvm-fullstack_developer_capstone/server/",'frontend/static')
+    os.path.join("/mnt/c/Users/sirib/Desktop/ibm/xrwvm-fullstack_developer_capstone/server/",'frontend/static'),
+    os.path.join("/mnt/c/Users/sirib/Desktop/ibm/xrwvm-fullstack_developer_capstone/server/",'frontend/build'),
+    os.path.join("/mnt/c/Users/sirib/Desktop/ibm/xrwvm-fullstack_developer_capstone/server/",'frontend/build/static')
 ]
 
